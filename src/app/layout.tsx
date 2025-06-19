dfbf15622b83dd2926c659a6fdd32df72ae2c83b
LunaFrancisco <francisco.luna@mail.udp.cl>
2025-06-18 23:12:46 -0400

    feat: add ClarityAnalytics component for enhanced analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { SessionProvider } from '@/components/SessionProvider'
 import { Toaster } from 'sonner'
 import { GoogleTagManager } from '@next/third-parties/google'
 import Clarity from '@microsoft/clarity';
+import ClarityAnalytics from "@/components/ClarityAnalytics";
 
 
 const inter = Inter({
@@ -26,6 +27,7 @@ export default function RootLayout({
   Clarity.init("s1nwh9nqgu");
   return (
     <html lang="es">
+      <ClarityAnalytics clarityId="s1nwh9nqgu" />
       <GoogleTagManager gtmId="GTM-PT38FR9F" />
       <body className={`${inter.className} min-h-screen flex flex-col`}>
         <SessionProvider>
